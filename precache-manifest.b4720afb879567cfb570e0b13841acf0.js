2ddefcece7f04cb73488e8b7ae0b0289688e05a0
Ashenafi <asheaddis@yahoo.com>
2019-09-16 14:15:20 +0200

    Updates

---
+++ precache-manifest.b4720afb879567cfb570e0b13841acf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267510892c94a5a9c30068ce6fc6d177",
+    "url": "./index.html"
+  },
+  {
+    "revision": "20676cd699b03734f0d2",
+    "url": "./static/css/main.eac4c621.chunk.css"
+  },
+  {
+    "revision": "518b64f0a5b93ad5ca4b",
+    "url": "./static/js/2.a9600128.chunk.js"
+  },
+  {
+    "revision": "20676cd699b03734f0d2",
+    "url": "./static/js/main.37d9f1b3.chunk.js"
+  },
+  {
+    "revision": "4884b8c1c250d151af0f",
+    "url": "./static/js/runtime~main.e7a0bebb.js"
+  }
+]);
