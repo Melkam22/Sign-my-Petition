d452095af28c9679c3fc03ff8ddf13c62b17f1c6
Ashenafi <asheaddis@yahoo.com>
2019-09-16 14:12:51 +0200

    sign petition form

---
+++ src/Components/Home.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Jumbotron, Container, Button } from 'reactstrap';
+
+
+class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+    render() {
+        return (
+            <div className="cont">
+                <Jumbotron fluid className="">
+                    <Container fluid>
+                        <h1 className="display-3">Home Page</h1>
+                        <p className="lead">Welcome & thank you for taking part
+                         in our petition. Good Luck!</p>
+                    </Container>
+                </Jumbotron>
+            </div>
+        )
+    }
+}
+
+export default Home;
